f95d8541b2aa6a40ffb415f457b5dd16602de0ee
BlindSecurity <62887679+Destroyer910@users.noreply.github.com>
2023-04-13 14:06:15 -0400

    Behold!

--- Assets/DanielStuff/Scripts/SS_Movement.cs
+++ Assets/DanielStuff/Scripts/SS_Movement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class SS_Movement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jump_height;
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float jump_height = 10;
+    private Animator anim;
     private Rigidbody2D body;
 
     private void Awake()
     {
+        // References
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.Space))
+        body.velocity = new Vector3(horizontalInput * speed, body.velocity.y);
+
+        // Texture flipper based on movement
+        if (horizontalInput > 0.01f) {
+            transform.localScale = Vector3.one;
+        } else if (horizontalInput < -0.01f) {
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
             body.velocity = new Vector2(body.velocity.x, jump_height);
+        }
+
+        // Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
     }
 }
