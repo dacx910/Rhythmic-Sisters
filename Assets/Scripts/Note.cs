743565c120a2727ad23f8cddfd95de4f53bbcf4b
4-KRussell <82389391+4-KRussell@users.noreply.github.com>
2023-05-08 14:16:15 -0400

    Stuff

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Note : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
     [SerializeField] float speed;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
@@ -22,5 +24,29 @@ public class Note : MonoBehaviour
     void Update()
     {
     
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Col1")
+        {
+            sr.color = new Color(255, 0, 0);
+        }
+        else if(col.gameObject.tag == "Col2")
+        {
+            sr.color = new Color(0, 255, 0);
+        }
+        else if(col.gameObject.tag == "Col3")
+        {
+            sr.color = new Color(0, 0, 255);
+        }
+        else if(col.gameObject.tag == "Col4")
+        {
+            sr.color = new Color(255, 0, 255);
+        }
+        else
+        {
+            Debug.Log("Something broke");
+        }
     }
 }
